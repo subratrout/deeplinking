9bee7c1892d8444f660febe7918f04fef5b21883
subratrout <subratnrout@gmail.com>
2016-01-27 10:21:03 -0800

    Added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root "parsings#new"
   resources :parsings
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
